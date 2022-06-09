3e7659b110d61c520702fc36f6c76bf15b893fb0
Joel Mero <joelXxgamexX@gmail.com>
2022-06-09 13:56:57 -0500

    Listo handgun

---
+++ js/updateLink.js
@@ -0,0 +1,8 @@
+let info = document.querySelector('#info'),
+	contacto = document.querySelector('#contacto');
+
+info.setAttribute('href', 'https://www.google.com'); // Agregar la URL de la pagina de git del proyecto
+info.setAttribute('target', '_blank');
+
+contacto.setAttribute('href', 'https://www.google.com'); // Agregar la URL de mi perfil de github
+contacto.setAttribute('target', '_blank');
