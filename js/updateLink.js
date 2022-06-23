21d84cba1056cdf814c1afc5850b3a8500caae1b
Joel Mero <joelXxgamexX@gmail.com>
2022-06-22 21:12:29 -0500

    Actualizados links

--- js/updateLink.js
+++ js/updateLink.js
@@ -1,8 +1,8 @@
 let info = document.querySelector('#info'),
 	contacto = document.querySelector('#contacto');
 
-info.setAttribute('href', 'https://www.joel.com'); // Agregar la URL de la pagina de git del proyecto
+info.setAttribute('href', 'https://github.com/KetteiPrOt/proyect_2'); // Agregar la URL de la pagina de git del proyecto
 info.setAttribute('target', '_blank');
 
-contacto.setAttribute('href', 'https://www.joel.com'); // Agregar la URL de mi perfil de github
+contacto.setAttribute('href', 'https://github.com/KetteiPrOt'); // Agregar la URL de mi perfil de github
 contacto.setAttribute('target', '_blank');
